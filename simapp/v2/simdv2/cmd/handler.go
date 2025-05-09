0fafb30dc03f31832493f2392ff798680bf426ee
Drogba <drogba2014@gmail.com>
2025-05-09 18:51:08 +0800

    chore: add debug log

--- simapp/v2/simdv2/cmd/handler.go
+++ simapp/v2/simdv2/cmd/handler.go
@@ -2,17 +2,16 @@ package cmd
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	"cosmossdk.io/core/transaction"
-	"cosmossdk.io/server/v2/cometbft"
 	"cosmossdk.io/server/v2/cometbft/handlers"
 	v1 "github.com/cometbft/cometbft/api/cometbft/abci/v1"
 )
 
 func CustomPrepareProposal[T transaction.Tx]() handlers.PrepareHandler[T] {
 	return func(ctx context.Context, app handlers.AppManager[T], codec transaction.Codec[T], req *v1.PrepareProposalRequest, chainID string) ([]T, error) {
+		fmt.Printf("CustomPrepareProposal, txs: %v\n", len(req.Txs))
 		var txs []T
 		for _, tx := range req.Txs {
 			decTx, err := codec.Decode(tx)
@@ -25,13 +24,14 @@ func CustomPrepareProposal[T transaction.Tx]() handlers.PrepareHandler[T] {
 		}
 
 		// "Process" vote extensions (we'll just inject all votes)
-		injectedTx, err := json.Marshal(req.LocalLastCommit)
-		if err != nil {
-			return nil, err
-		}
+		// injectedTx, err := json.Marshal(req.LocalLastCommit)
+		// if err != nil {
+		// 	return nil, err
+		// }
 
-		// put the injected tx into the first position
-		txs = append([]T{cometbft.RawTx(injectedTx).(T)}, txs...)
+		// // put the injected tx into the first position
+		// txs = append([]T{cometbft.RawTx(injectedTx).(T)}, txs...)
+		fmt.Printf("CustomPrepareProposal appended, txs: %v\n", len(txs))
 
 		return txs, nil
 	}
